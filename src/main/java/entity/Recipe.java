f3066752552b7446a04b05d7f76e1f82f0bddb94
sinasara <sara.sina@mail.utoronto.ca>
2025-07-24 15:48:57 -0400

    Finished Recipe class

--- src/main/java/entity/Recipe.java
+++ src/main/java/entity/Recipe.java
@@ -6,5 +6,34 @@ package entity;
  * imageurl, nutrition, authorId, isPublic (?)
  */
 public class Recipe {
+    private String id;
+    private String name;
+    private List<String> ingredients;
+    private String steps;
+    private String authorId;
+
+    public Recipe(String id, String name, List<String> ingredients, String steps, String authorId) {
+        this.id = id;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.steps = steps;
+        this.authorId = authorId;
+    }
+
+    public String getId() {return id;}
+    public void setId(String id) {this.id = id;}
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public List<String> getIngredients() {return ingredients;}
+    public void setIngredients(List<String> ingredients) {this.ingredients = ingredients;}
+
+    public String getSteps() {return steps;}
+    public void setSteps(String steps) {this.steps = steps;}
+
+    public String getAuthorId() {return authorId;}
+    public void setAuthorId(String authorId) {this.authorId = authorId;}
+
 
 }
