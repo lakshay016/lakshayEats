c29e21d1f3026df9365d897d6f0137a21e387952
guardtia] <[tiana.guard@mail.utoronto.ca]>
2025-07-24 12:46:00 -0400

    User Story 4: Created Inbox functionality and message/Recipe sending functionality

--- src/main/java/entity/CommonUser.java
+++ src/main/java/entity/CommonUser.java
@@ -1,12 +1,17 @@
 package entity;
+import java.util.ArrayList;
 
 public class CommonUser implements User{
     private final String userName;
     private final String password;
+    public final ArrayList<Recipe> inbox;
+    public final ArrayList<User> friends;
 
     public CommonUser(String userName, String password) {
         this.userName = userName;
         this.password = password;
+        this.inbox = new ArrayList<>();
+        this.friends = new ArrayList<>();
     }
 
     @Override
@@ -19,5 +24,30 @@ public class CommonUser implements User{
         return password;
     }
 
-}
+    @Override
+    public Object sendMessage(User receiver, String message) {
+        if (this.friends.contains(receiver)){
+        User.inbox.add(this.getUsername() + ": " + message);
+        return null;
+        }
+        else{
+            return ("Add as a friend first!");
+        }
+
+    }
+
+    @Override
+    public Object sendRecipe(User receiver, Recipe recipe) {
+        if (this.friends.contains(receiver)){
+            User.inbox.add(this.getUsername() + ": " + recipe);
+            return null;
+        }
+        else{
+            return ("Add as a friend first!");
+        }
+
+    }
+
+    }
+
 
--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,10 +1,18 @@
 package entity;
+import java.util.ArrayList;
 
 /**
  * registered user
  */
 public interface User {
+    ArrayList<String> inbox = new ArrayList<>();
+
+
+
 
     String getUsername();
     String getPassword();
+    Object sendMessage(User receiver, String message);
+    Object sendRecipe(User receiver, Recipe recipe);
+
 }
