00971d324b746a803275110fa417a8d3ee8c3979
sinasara <sara.sina@mail.utoronto.ca>
2025-07-25 20:04:59 -0400

    Completed RecipeEditDataAccessInterface

--- src/main/java/use_case/RecipeEdit/RecipeDataAccessInterface.java
+++ src/main/java/use_case/RecipeEdit/RecipeDataAccessInterface.java
@@ -1,5 +1,11 @@
 package use_case.RecipeEdit;
 
+import entity.Recipe;
+
 public interface RecipeDataAccessInterface {
+    Recipe getRecipe(int id) throws Exception;
+    void edit(Recipe recipe) throws Exception;
+    void save(Recipe recipe) throws Exception;
+
 
 }
