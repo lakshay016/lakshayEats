56265bf03b1d8e3c8043754b6baf0489542fcc66
Oracle <tiana.guard@mail.utoronto.ca>
2025-07-31 15:54:04 -0400

    updated commonuser

--- src/main/java/entity/CommonUser.java
+++ src/main/java/entity/CommonUser.java
@@ -1,103 +1,175 @@
 package entity;
+import app.LoginTestDriver;
+import data_access.DBUserDataAccessObject;
+import org.json.JSONException;
+import org.json.JSONObject;
+import use_case.login.LoginUserDataAccessInterface;
+import data_access.DBFriendDataAccessObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Set;
 
 public class CommonUser implements User{
     private final String userName;
     private final String password;
     public ArrayList <Object>inbox;
-    public final ArrayList<User> friends;
-    public final ArrayList<User> blockedList;
-    public ArrayList<User> friendRequests;
 
     public CommonUser(String userName, String password) {
         this.userName = userName;
         this.password = password;
-        this.inbox = new ArrayList<>();
-        this.friends = new ArrayList<>();
-        this.blockedList = new ArrayList<>();
-        this.friendRequests = new ArrayList<>();
+
+    }
+    DBFriendDataAccessObject db = new DBFriendDataAccessObject();
+
+    public Set<String> getFriends() throws IOException {
+        String testUsername = this.userName;
+        JSONObject json = db.fetch(testUsername);
+
+        if (json == null) {
+            System.out.println("No friend data found for username: " + testUsername);
+        } else {
+            System.out.println("Friend data found!");
+            return db.toSet(json.getJSONArray("friends"));
+        }
+        return null;
+    }
+    Set<String> getRequests() throws IOException {
+        String testUsername = this.userName;
+        JSONObject json = db.fetch(testUsername);
+
+        if (json == null) {
+            System.out.println("No friend data found for username: " + testUsername);
+        } else {
+            System.out.println("Friend data found!");
+            return db.toSet(json.getJSONArray("requests"));
+        }
+        return null;
+    }
+    Set<String> getBlocked() throws IOException{
+        String testUsername = this.userName;
+        JSONObject json = db.fetch(testUsername);
+
+        if (json == null) {
+            System.out.println("No friend data found for username: " + testUsername);
+        } else {
+            System.out.println("Friend data found!");
+            return db.toSet(json.getJSONArray("blocked"));
+        }
+        return null;
     }
 
-    @Override
+
     public String getUsername() {
         return userName;
     }
 
-    @Override
     public String getPassword() {
         return password;
     }
 
-    @Override
     public void sendMessage(CommonUser receiver, String message) {
-        if (this.friends.contains(receiver)){
-        User.inbox.add(this.getUsername() + ": " + message);
-        }
-        else{
-            System.out.println("Add as a friend first!");
-        }
+
     }
 
-    @Override
     public void sendRecipe(CommonUser receiver, Recipe recipe) {
-        if (this.friends.contains(receiver)){
-            receiver.inbox.add(this.getUsername() + ": " + recipe);
-        }
-        else{
-            System.out.println("Add as a friend first!");
-        }
+
     }
 
-    @Override
+
     public void addFriend(CommonUser friend) {
-        if (friend.blockedList.contains(this)){
-            System.out.println("Friend Request Sent!");
-        if (friend.friendRequests.contains(this)){
-            System.out.println("Friend Request Has Already Been Sent!");
+
+    }
+
+    public void RequestFriend(CommonUser x) throws IOException {
+        String testUsername = x.userName;
+        JSONObject json = db.fetch(testUsername);
+
+        if (json == null) {
+            System.out.println("No friend data found for username: " + testUsername);
+        } else {
+            System.out.println("Friend data found!");
+            Set<String> xfriends = db.toSet(json.getJSONArray("friends"));
+            Set<String> xrequests = db.toSet(json.getJSONArray("requests"));
+            Set<String> xblocked = db.toSet(json.getJSONArray("blocked"));
+
+            if (xblocked.contains(this.userName)) {
+                System.out.println("Error Processing Request");}
+            else if (xrequests.contains(this.userName)) {
+                System.out.println("Friend Request Has Already Been Sent!");}
+            else if (xfriends.contains(this.userName)) {
+                System.out.println("Already Friends with User!");}
+            else {xrequests.add(this.userName);
+                System.out.println("Request Sent!");
+                db.save(x.userName,xfriends,xrequests,xblocked);
+            }
         }
-        if (this.friends.contains(friend)){
-            System.out.println("This User Has already Requested to Friend you! Please Accept" +
-                    "The Friend Request In Your Requests Tab!");
-        }
-        if (this.friends.contains(friend)){
-            System.out.println("Already Friends with User!");
-        }
-        else{
-            friend.inbox.add(this);
-        }
-    }
     }
 
-    public void removeFriend(CommonUser friend) {
-        this.friends.remove(friend);
-    }
+    public void removeFriend(CommonUser y) throws IOException {
+        String testUsername = y.userName;
+        JSONObject json = db.fetch(testUsername);
 
-    @Override
-    public void blockUser(CommonUser user) {
-        this.blockedList.add(user);
-        this.friends.remove(user);
-    }
-
-    @Override
-    public void unblockUser(CommonUser friend) {
-        this.blockedList.remove(friend);
-    }
-
-    @Override
-    public void acceptFriend(CommonUser friend) {
-        if (this.friendRequests.contains(friend)){
-            this.friends.add(friend);
-            friend.friends.add(this);
+        if (json == null) {
+            System.out.println("No friend data found for username: " + testUsername);
+        } else {
+            System.out.println("Friend data found!");
+            Set<String> yfriends = db.toSet(json.getJSONArray("friends"));
+            Set<String> yrequests = db.toSet(json.getJSONArray("requests"));
+            Set<String> yblocked = db.toSet(json.getJSONArray("blocked"));
+            yfriends.remove(this.userName);
+            Set<String> thisFriends = this.getFriends();
+            Set<String> thisRequests = this.getRequests();
+            Set<String> thisBlocked = this.getBlocked();
+            this.getFriends().remove(y.userName);
+            db.save(y.userName, yfriends, yrequests, yblocked);
+            db.save(this.userName, thisFriends, thisRequests, thisBlocked);
         }
 
-    }
-
-    @Override
-    public void rejectFriend(CommonUser friend) {
-        this.friendRequests.remove(friend);
 
     }
 
-}
+    public void blockUser(CommonUser user) throws IOException {
+        this.getBlocked().add(user.getUsername());
+        this.getFriends().remove(user.getUsername());
+        db.save(this.userName, this.getFriends(), this.getRequests(), this.getBlocked());
+
+    }
+
+    public void unblockUser(CommonUser friend) throws IOException {
+        this.getBlocked().remove(friend.userName);
+        db.save(this.userName, this.getFriends(), this.getRequests(), this.getBlocked());
+    }
+
+    public void acceptFriend(CommonUser friend) throws IOException {
+        String testUsername = friend.userName;
+        JSONObject json = db.fetch(testUsername);
+
+        if (json == null) {
+            System.out.println("No friend data found for username: " + testUsername);
+        } else {
+            System.out.println("Friend data found!");
+            Set<String> ffriends = db.toSet(json.getJSONArray("friends"));
+            Set<String> frequests = db.toSet(json.getJSONArray("requests"));
+            Set<String> fblocked = db.toSet(json.getJSONArray("blocked"));
+            ffriends.add(this.userName);
+            this.getFriends().add(friend.userName);
+            this.getRequests().remove(friend.userName);
+            frequests.remove(this.userName);
+            db.save(this.userName, this.getFriends(), this.getRequests(), this.getBlocked());
+            db.save(friend.userName, ffriends, frequests, fblocked);
+        }}
+
+    public void rejectFriend(CommonUser friend) throws IOException {
+        this.getRequests().remove(friend.userName);
+        db.save(this.userName, this.getFriends(), this.getRequests(), this.getBlocked());
+
+    }}
+
+        /*
+        TEST THE SHIT
+        MAKE A SEARCH FUNCTION FOR USERS
+        CLEAN ARCHITECTURE!!!
+         */
 
 
--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -5,22 +5,12 @@ import java.util.ArrayList;
  * registered user
  */
 public interface User {
-    ArrayList<String> inbox = new ArrayList<>();
-    ArrayList<String> friendList = new ArrayList<>();
-    ArrayList<String> blockedList = new ArrayList<>();
 
 
 
 
     String getUsername();
     String getPassword();
-    void sendMessage(CommonUser receiver, String message);
-    void sendRecipe(CommonUser receiver, Recipe recipe);
-    void addFriend(CommonUser friend);
-    void removeFriend(CommonUser friend);
-    void blockUser(CommonUser friend);
-    void unblockUser(CommonUser friend);
-    void acceptFriend(CommonUser friend);
-    void rejectFriend(CommonUser friend);
+
 
 }
